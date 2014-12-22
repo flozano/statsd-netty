8526977e2ade4b7b5d4f157b088cec5ef63dd7f2
Francisco A. Lozano <flozano@gmail.com>
2014-12-22 06:50:21 +0100

    Additional constructors

--- src/main/java/com/flozano/statsd/metrics/Gauge.java
+++ src/main/java/com/flozano/statsd/metrics/Gauge.java
@@ -5,6 +5,10 @@ public class Gauge extends Metric {
 	public Gauge(String name, long value, Double sample) {
 		super(name, value, sample);
 	}
+	
+	public Gauge(String name, long value) {
+		this(name, value, null);
+	}
 
 	@Override
 	public String getSuffix() {
--- src/main/java/com/flozano/statsd/metrics/Timing.java
+++ src/main/java/com/flozano/statsd/metrics/Timing.java
@@ -5,6 +5,10 @@ public class Timing extends Metric {
 	public Timing(String name, long value, Double sample) {
 		super(name, value, sample);
 	}
+	
+	public Timing(String name, long value) {
+		this(name, value, null);
+	}
 
 	@Override
 	public String getSuffix() {
