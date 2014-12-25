686376cfb2faaa9104ee09da53cc99f78abf6ff1
Francisco A. Lozano <flozano@gmail.com>
2014-12-26 00:31:49 +0100

    Separate instantiation in test

--- src/test/java/com/flozano/statsd/netty/IntegrationTest.java
+++ src/test/java/com/flozano/statsd/netty/IntegrationTest.java
@@ -3,6 +3,7 @@ package com.flozano.statsd.netty;
 import static com.jayway.awaitility.Awaitility.await;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.everyItem;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 import java.util.concurrent.TimeUnit;
@@ -14,13 +15,13 @@ import com.flozano.statsd.mock.DummyUDPServer;
 
 public class IntegrationTest {
 
-	static int NUMBER_OF_ITEMS = 2500;
+	static final int PORT = 8125;
+	static final int NUMBER_OF_ITEMS = 2500;
 
 	@Test
 	public void test() throws Exception {
-		try (DummyUDPServer server = new DummyUDPServer(8125)) {
-			try (NettyStatsDClientImpl c = new NettyStatsDClientImpl(
-					"127.0.0.1", 8125)) {
+		try (DummyUDPServer server = newServer()) {
+			try (NettyStatsDClientImpl c = newClient()) {
 				for (int i = 0; i < NUMBER_OF_ITEMS; i++) {
 					c.send(new Count("example", 1));
 				}
@@ -28,7 +29,16 @@ public class IntegrationTest {
 						.until(() -> server.getItemsSnapshot().size() == NUMBER_OF_ITEMS);
 				assertThat(server.getItemsSnapshot(),
 						everyItem(equalTo("example:1|c")));
+				assertEquals(NUMBER_OF_ITEMS, server.getItemsSnapshot().size());
 			}
 		}
+	}
+
+	private NettyStatsDClientImpl newClient() {
+		return new NettyStatsDClientImpl("127.0.0.1", PORT);
+	}
+
+	private DummyUDPServer newServer() {
+		return new DummyUDPServer(PORT);
 	}
 }
