00f7d86c06adbad84b4221b7fee7275d85728ea0
Francisco A. Lozano <flozano@gmail.com>
2014-12-26 10:50:19 +0100

    Added stupid javadoc

--- src/main/java/com/flozano/statsd/StatsDClient.java
+++ src/main/java/com/flozano/statsd/StatsDClient.java
@@ -6,6 +6,12 @@ import com.flozano.statsd.metrics.Metric;
 
 public interface StatsDClient {
 
+	/**
+	 * Send a bunch of metrics to StatsD server.
+	 * 
+	 * @param metrics
+	 * @return
+	 */
 	CompletableFuture<Void> send(Metric... metrics);
 
 }
